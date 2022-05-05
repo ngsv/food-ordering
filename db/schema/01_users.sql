675c73005c606cc7e2c7ffda62f5f184470ee29d
Steven Ngov <gl.steven.ngov@gmail.com>
2022-05-05 16:24:29 +0000

    Update ERD, created database schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,6 +1,9 @@
 -- Drop tables
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS menu_items CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
 
 -- Recreate tables
 CREATE TABLE users (
@@ -12,4 +15,33 @@ CREATE TABLE users (
   phone_number VARCHAR(32) NOT NULL,
   email VARCHAR(255) NOT NULL,
   is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER,
+  order_time TIMESTAMP,
+  expected_pickup_time TIMESTAMP,
+  total_amount DECIMAL,
+  is_complete BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_name VARCHAR(255)
+);
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price DECIMAL,
+  image_url VARCHAR(255),
+  category_id INTEGER
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER,
+  menu_item_id INTEGER
 );
